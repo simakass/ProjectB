6ff7fee22cf86f8311371f8b02782e8aad4af965
Kevin Wong <86650608+KevinWongCS@users.noreply.github.com>
2022-04-01 18:25:06 -0700

    Update LinkedFrontBackCappedList.java
    Added names, filename, date, and description to header.

--- LinkedFrontBackCappedList.java
+++ LinkedFrontBackCappedList.java
@@ -1,3 +1,9 @@
+/* group names: <add your name>, <add your name>, Christian Kurt Balais, Kevin Wong
+ * filename: LinkedFrontBackCappedList.java
+ * date: 3/29/2022
+ * desc: Class implementation of FrontBackCappedListInterface<T> utilizing linked nodes.
+ */
+
 public class LinkedFrontBackCappedList<T> implements FrontBackCappedListInterface<T> {
 
 	private Node head, tail;
